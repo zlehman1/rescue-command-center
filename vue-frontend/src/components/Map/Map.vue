c7d1d21fa4950e2481080fa4904838074c315308
Bastian Arping <bastian.arping@outlook.de>
2024-06-25 20:27:19 +0200

    🎨 styled map

--- vue-frontend/src/components/Map/Map.vue
+++ vue-frontend/src/components/Map/Map.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import Header from "../menu/Header.vue";
 import { useI18n } from 'vue-i18n';
 import Footer from "../../components/menu/Footer.vue";
 import MapComponent from "./MapComponent.vue";
 
 const { t } = useI18n();
-const mapWidth = ref(window.innerWidth * 0.8);
-const mapHeight = ref(window.innerHeight * 0.8);
-const location = ref('Düsseldorf, Germany');
+const mapWidth = ref(1200);
+const mapHeight = ref(600);
+const location = ref('');
 
 const updateMapSize = () => {
   mapWidth.value = window.innerWidth * 0.8;
@@ -16,6 +16,11 @@ const updateMapSize = () => {
 };
 
 window.addEventListener('resize', updateMapSize);
+
+watch(location, (newLocation) => {
+  console.log('Location updated to:', newLocation);
+});
+
 </script>
 
 <template>
@@ -23,7 +28,23 @@ window.addEventListener('resize', updateMapSize);
     <Header :componentName="t('mapTitle')"/>
     <v-main class="main-content">
       <div class="map-container">
-        <MapComponent :height="mapHeight" :width="mapWidth" :location="location" />
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                  v-model="location"
+                  :label="t('Enter Location')"
+                  outlined
+                  dense
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <MapComponent :height="mapHeight" :width="mapWidth" :location="location" />
+            </v-col>
+          </v-row>
+        </v-container>
       </div>
     </v-main>
     <Footer/>
@@ -32,18 +53,19 @@ window.addEventListener('resize', updateMapSize);
 
 <style scoped>
 .main-content {
+  padding-top: 4rem; /* Füge oben ausreichend Abstand hinzu, um den Header nicht zu überlappen */
   padding-bottom: 3rem;
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: 100vh;
+  align-items: flex-start; /* Ändere von center zu flex-start, um den Inhalt von oben zu beginnen */
+  min-height: calc(100vh - 8rem); /* Passe die minimale Höhe an, um Header und Footer zu berücksichtigen */
 }
 
 .map-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
 }
 </style>

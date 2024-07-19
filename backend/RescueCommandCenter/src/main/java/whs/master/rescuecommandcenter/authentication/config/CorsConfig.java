3c3a77ae169cdc8564320c4454e7ad1540909532
Bastian Arping <bastian.arping@outlook.de>
2024-07-19 16:28:47 +0200

    🔧 changed allowed origins

--- backend/RescueCommandCenter/src/main/java/whs/master/rescuecommandcenter/authentication/config/CorsConfig.java
+++ backend/RescueCommandCenter/src/main/java/whs/master/rescuecommandcenter/authentication/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);

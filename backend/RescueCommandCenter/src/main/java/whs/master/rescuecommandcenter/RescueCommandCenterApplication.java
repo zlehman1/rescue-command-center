c03b583642660af57e098b2e6146e21f3baa77c4
Bastian Arping <bastian.arping@outlook.de>
2024-07-24 14:03:04 +0200

    🎨 removed hardcoded neo4j configuration and added it to env file

--- backend/RescueCommandCenter/src/main/java/whs/master/rescuecommandcenter/RescueCommandCenterApplication.java
+++ backend/RescueCommandCenter/src/main/java/whs/master/rescuecommandcenter/RescueCommandCenterApplication.java
@@ -1,7 +1,13 @@
 package whs.master.rescuecommandcenter;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
@@ -9,5 +15,14 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 public class RescueCommandCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(RescueCommandCenterApplication.class, args);
+    }
+
+    @Bean
+    public Driver neo4jDriver(Dotenv dotenv) {
+        String uri = String.format("bolt://%s:%s", dotenv.get("NEO4J_HOST"), dotenv.get("NEO4J_PORT"));
+        return GraphDatabase.driver(uri, org.neo4j.driver.AuthTokens.basic(
+                dotenv.get("NEO4J_USERNAME"),
+                dotenv.get("NEO4J_PASSWORD")
+        ));
     }
 }
